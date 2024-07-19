9c57afb20402a30f377abb3d326f4155eb2a30cc
leo-cal <calasans.leonardo@gmail.com>
2024-07-19 17:49:09 -0300

    Music Pearls Data Parser

---
+++ config.py
@@ -0,0 +1,31 @@
+
+# MusicPearls
+CLIENT_ID = ''
+CLIENT_SECRET = ''
+ACTOKEN = ''
+RFTOKEN = ''
+
+# MusicPearls_tracks
+CLIENT_ID_1 = ''
+CLIENT_SECRET_1 = ''
+ACTOKEN_1 = ''
+RFTOKEN_1 = ''
+
+# MusicPearls_tracks2
+CLIENT_ID_2 = ''
+CLIENT_SECRET_2 = ''
+ACTOKEN_2 = ''
+RFTOKEN_2 = ''
+
+
+# MusicPearls_tracks3
+CLIENT_ID_3 = ''
+CLIENT_SECRET_3 = ''
+ACTOKEN_3 = ''
+RFTOKEN_3 = ''
+
+# MusicPearls_tracks4
+CLIENT_ID_4 = ''
+CLIENT_SECRET_4 = ''
+ACTOKEN_4 = ''
+RFTOKEN_4 = ''
